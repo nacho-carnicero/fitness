f8482d6b00089d9d7f008c57aeeb850867ab8465
Nacho Carnicero <ignacio.carnicero@sterblue.com>
2020-04-11 19:30:07 +0100

    Added storybook

---
+++ .storybook/main.js
@@ -0,0 +1,8 @@
+module.exports = {
+  stories: ['../src/**/__stories__/*'],
+  addons: [
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+  ],
+};
