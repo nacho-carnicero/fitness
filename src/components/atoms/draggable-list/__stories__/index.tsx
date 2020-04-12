46341baa25fb073c4b94c1e4e9503420c1d4d555
Nacho Carnicero <ignacio.carnicero@sterblue.com>
2020-04-12 19:44:39 +0100

    Make draggable list be managed from outside

--- src/components/atoms/draggable-list/__stories__/index.tsx
+++ src/components/atoms/draggable-list/__stories__/index.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import styled from "@emotion/styled";
+import { DragDropContext } from "react-beautiful-dnd";
 import { DraggableList } from "../";
 
 export default {
@@ -8,7 +9,7 @@ export default {
 };
 
 const grid = 8;
-const QuoteItem = styled.div`
+const StyledElement = styled.div`
   width: 200px;
   border: 1px solid grey;
   margin-bottom: ${grid}px;
@@ -16,12 +17,54 @@ const QuoteItem = styled.div`
   padding: ${grid}px;
 `;
 
+const reorder = (
+  list: React.FC[],
+  startIndex: number,
+  endIndex: number
+): React.FC[] => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
+};
+
 export const ExampleList = () => {
-  const [items, setItems] = useState({});
+  const intialItems = [
+    <StyledElement key="element0">Element 0</StyledElement>,
+    <StyledElement key="element1">Element 1</StyledElement>,
+    <StyledElement key="element2">Element 2</StyledElement>,
+    <StyledElement key="element3">Element 3</StyledElement>,
+    <StyledElement key="element4">Element 4</StyledElement>,
+    <StyledElement key="element5">Element 5</StyledElement>,
+    <StyledElement key="element6">Element 6</StyledElement>,
+    <StyledElement key="element7">Element 7 </StyledElement>
+  ];
+  const [items, setItems] = useState(intialItems);
+
+  const onDragEnd = useCallback(
+    result => {
+      if (!result.destination) {
+        return;
+      }
+
+      if (result.destination.index === result.source.index) {
+        return;
+      }
+
+      const newItems = reorder(
+        items,
+        result.source.index,
+        result.destination.index
+      );
+      setItems(newItems);
+    },
+    [items]
+  );
+
   return (
-    <DraggableList>
-      <QuoteItem />
-      <QuoteItem />
-    </DraggableList>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <DraggableList listId={"list"}>{items}</DraggableList>
+    </DragDropContext>
   );
 };
--- src/components/atoms/draggable-list/index.tsx
+++ src/components/atoms/draggable-list/index.tsx
@@ -1,97 +1,45 @@
-import React, { useState, ReactComponentElement } from "react";
-import ReactDOM from "react-dom";
+import React from "react";
+import { map, flatten } from "lodash/fp";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
-// import type { Quote as QuoteType } from "../types";
 
-const initial = Array.from({ length: 10 }, (v, k) => k).map(k => {
-  const custom: Quote = {
-    id: `id-${k}`,
-    content: `Quote ${k}`
-  };
+const mapUncapped = map.convert({ cap: false });
+type Item = React.FC;
 
-  return custom;
-});
-
-const grid = 8;
-const reorder = (list, startIndex, endIndex) => {
-  const result = Array.from(list);
-  const [removed] = result.splice(startIndex, 1);
-  result.splice(endIndex, 0, removed);
-
-  return result;
-};
-
-// const QuoteItem = styled.div`
-//   width: 200px;
-//   border: 1px solid grey;
-//   margin-bottom: ${grid}px;
-//   background-color: lightblue;
-//   padding: ${grid}px;
-// `;
-
-// function Quote({ quote, index }) {
-//   return (
-//     <Draggable draggableId={quote.id} index={index}>
-//       {provided => (
-//         <QuoteItem
-//           ref={provided.innerRef}
-//           {...provided.draggableProps}
-//           {...provided.dragHandleProps}
-//         >
-//           {quote.content}
-//         </QuoteItem>
-//       )}
-//     </Draggable>
-//   );
-// }
-
-const ItemsList = React.memo(function ItemsList({ items }) {
-  return items.map((Item: React.FC, index: number) => (
-    <Draggable draggableId={`${index}`} index={index}>
-      {provided => (
-        <div
-          ref={provided.innerRef}
-          {...provided.draggableProps}
-          {...provided.dragHandleProps}
-        >
-          {Item}
-        </div>
-      )}
-    </Draggable>
-  ));
-});
-
-export function DraggableList(props) {
-  const [state, setState] = useState({ quotes: initial });
-
-  function onDragEnd(result) {
-    if (!result.destination) {
-      return;
-    }
-
-    if (result.destination.index === result.source.index) {
-      return;
-    }
-
-    const quotes = reorder(
-      state.quotes,
-      result.source.index,
-      result.destination.index
-    );
-
-    setState({ quotes });
-  }
-  console.log("PROPS", props);
-  return (
-    <DragDropContext onDragEnd={onDragEnd}>
-      <Droppable droppableId="list">
+const ItemsList = React.memo(function ItemsList({ items }: { items: Item[] }) {
+  return mapUncapped(
+    (Item: React.FC, index: number) => (
+      <Draggable draggableId={`${index}`} index={index} key={index}>
         {provided => (
-          <div ref={provided.innerRef} {...provided.droppableProps}>
-            <ItemsList items={props.children} />
-            {provided.placeholder}
+          <div
+            ref={provided.innerRef}
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+          >
+            {Item}
           </div>
         )}
-      </Droppable>
-    </DragDropContext>
+      </Draggable>
+    ),
+    items
+  );
+});
+
+type props = {
+  listId: string;
+};
+
+export function DraggableList(props: props) {
+  const items = flatten([props.children]);
+  const listId = props.listId;
+
+  return (
+    <Droppable droppableId={listId}>
+      {provided => (
+        <div ref={provided.innerRef} {...provided.droppableProps}>
+          <ItemsList items={items} />
+          {provided.placeholder}
+        </div>
+      )}
+    </Droppable>
   );
 }
