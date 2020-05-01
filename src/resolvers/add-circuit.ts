03592adf87c451091dfb7085bd413cbd366619b8
Nacho Carnicero <i.carnicerotresca@gmail.com>
2020-05-02 00:27:34 +0100

    Added some doc

--- src/resolvers/add-circuit.ts
+++ src/resolvers/add-circuit.ts
@@ -4,6 +4,11 @@ import { Training, CircuitInputToAddCircuit } from "../types";
 
 const defaultEmptyCircuit = { type: "circuit", plan: [] };
 
+/**
+ * Function that adds a circuit to the current training plan
+ * @param previousTraining training object before adding the new circuit
+ * @param newCircuit circuit object to be added to the training
+ */
 export const addCircuit = (
   previousTraining: Training,
   newCircuit: CircuitInputToAddCircuit = defaultEmptyCircuit
--- src/resolvers/remove-circuit.ts
+++ src/resolvers/remove-circuit.ts
@@ -1,6 +1,11 @@
 import { filter, getOr } from "lodash/fp";
 import { Training, Circuit } from "../types";
 
+/**
+ * Function that deletes a circuit in the current training plan
+ * @param previousTraining training object before deleting the circuit
+ * @param circuitId id of the circuit to be deleted
+ */
 export const removeCircuit = (
   previousTraining: Training,
   circuitId: string
